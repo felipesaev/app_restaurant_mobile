bc082979a601c3395e50bb3246e3c1c9fddeb7b0
Felipe Costa <felipesaev@gmail.com>
2022-12-15 10:10:36 -0300

    feat: create project and configuration rules

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { Text } from "./src/components/Text";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+  });
+
+  if (!isFontLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text size={24} weight="700" opacity={0.5} color="red">
+        Primeira experiência com Native
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
