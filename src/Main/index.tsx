a164c25e6fd446979a49866ee0c06fe31a2c0dc9
Felipe Costa <felipesaev@gmail.com>
2023-01-18 08:48:13 -0300

    feat: create component button for foooter page

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,20 +1,32 @@
-import {  Container, CategoriesContainer, MenuContainer, FooterContainer } from './styles';
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  FooterContainer,
+  Footer,
+} from './styles';
 
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
+import { Button } from '../components/Button';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
+    <>
+      <Container>
+        <Header />
 
-      <CategoriesContainer>
-        <Categories />
-      </CategoriesContainer>
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
 
-      <MenuContainer></MenuContainer>
-
-      <FooterContainer></FooterContainer>
-    </Container>
+        <MenuContainer></MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer>
+          <Button onPress={() => alert('teste')}>Novo Pedido</Button>
+        </FooterContainer>
+      </Footer>
+    </>
   );
 }
--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -4,11 +4,21 @@ import { Platform, StatusBar } from 'react-native';
 const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.SafeAreaView`
+  flex: 1;
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
 `;
 
 export const CategoriesContainer = styled.View`
-  height: 144px;
+  height: 73px;
+  margin-top: 34px;
 `;
-export const MenuContainer = styled.View``;
-export const FooterContainer = styled.View``;
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+export const FooterContainer = styled.SafeAreaView``;
+
+export const Footer = styled.View`
+  padding: 16px 24px;
+  min-height: 110px;
+  background: #fff;
+`;
